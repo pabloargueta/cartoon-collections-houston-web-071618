fef9a43eba5f7d1fbd77b02c324f370256cb9997
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-03 05:14:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ def long_planeteer_calls(array)
   # Your code here
 end
 
-def find_the_cheese(array_calls)
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array_calls.any? { |call| call.length >= 4 }
+  array.any? { |call| call.length >= 4 }
 end
