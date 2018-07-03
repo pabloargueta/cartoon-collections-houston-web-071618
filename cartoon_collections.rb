0c5cec9a54c89324dbf0c9cc3ac7439e231cbf99
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-03 05:06:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 
 def summon_captain_planet(array)
   
-  array.map { |name| "#{name.capitalize}!
-  "
+  array.map { |name| "#{name.capitalize}!"}
+  
 end
 
 def long_planeteer_calls# code an argument here
